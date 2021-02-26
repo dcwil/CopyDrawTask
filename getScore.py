d9fce33bd31ce248788a699670d861fa4640a110
dcwil <dan.c.wil@gmail.com>
2021-02-26 16:06:16 +0100

    ported get score properly (some verification done)

--- getScore.py
+++ getScore.py
@@ -8,24 +8,116 @@ import numpy as np
 from pixToIm import pixToIm
 
 
+### not yet written in the statistics on individual trials thats in the mat file
+
+def getScore(gaus_template,gaus_trace):
+    
+    youSuck = np.sum(gaus_template)
+    score = np.sum(np.abs(gaus_template-gaus_trace))
+    score = score/youSuck 
+    
+    if score > 1: score = 1
+    
+    score = 1-score
+    score = score*100
+    return score
+
+
+#wrap pixtioim and getscore into one
+def get_score_from_trace(trace,template,thebox,winsize,shift=True):
+    
+    gaus_template,_,_ = pixToIm(template,thebox,winsize,shift=shift)
+    gaus_trace,_,_ = pixToIm(trace,thebox,winsize,shift=shift)
+    
+    return getScore(gaus_template,gaus_trace)
 
 if __name__ == '__main__':
-    import matplotlib.pyplot as plt
-    import pandas as pd
-    df = pd.read_pickle('results/TEST_SESSION/TEST_BLOCK/scores_copyDraw_block1.pkl')
+    #import matplotlib.pyplot as plt
+    #import pandas as pd
     
-    pixLet = df.loc['pos_t'][0]
-    theBox = df.loc['theBoxPix'][0]
-    winSize = df.loc['winSize'][0]
-    target = df.loc['templatePix'][0]
+    # df = pd.read_pickle('results/TEST_SESSION/TEST_BLOCK/scores_copyDraw_block1.pkl')
     
-    gaus1,im1,blur1 = pixToIm(pixLet,theBox,winSize)
-    gaus2,im2,blur2 = pixToIm(target,theBox,winSize)
+    # trial_no = 0
     
-    fig, ax = plt.subplots(nrows=2,ncols=2)
+    # pixLet = df.loc['pos_t'][trial_no]
+    # theBox = df.loc['theBoxPix'][trial_no]
+    # winSize = df.loc['winSize'][trial_no]
+    # target = df.loc['templatePix'][trial_no]
     
-    ax[0,0].plot(pixLet.T[0],pixLet.T[1])
-    ax[1,0].plot(target.T[0],target.T[1])
+    # gaus1,im1,blur1 = pixToIm(target,theBox,winSize)
+    # gaus2,im2,blur2 = pixToIm(pixLet,theBox,winSize) #trace
     
-    ax[0,1].imshow(gaus1)
-    ax[1,1].imshow(gaus2)
+    
+    
+    # #sanity check
+    # fig, ax = plt.subplots(nrows=2,ncols=2)
+    
+    # ax[0,0].plot(pixLet.T[0],pixLet.T[1])
+    # ax[1,0].plot(target.T[0],target.T[1])
+    
+    # ax[0,1].imshow(gaus1)
+    # ax[1,1].imshow(gaus2)
+    
+    
+    # youSuck = np.sum(gaus1)
+    # score = np.sum(np.abs(gaus1-gaus2))
+    # score = score/youSuck
+    
+    # print(score)
+    
+    # #cut off the score if its too high (high =bad?)
+    # if score > 1:
+    #     score = 1
+        
+    # score = 1-score
+    # score = score*100
+    # print(score)
+    
+    
+    #verify with matlab
+    from pathlib import Path
+    import scipy.io as sio
+    
+    
+    #matlab results
+    for s in [1,2,3]:
+        p = Path(rf'F:\F_WSL\AG Tangermann\CopyDraw\CopyDraw_mat_repo\CopyDrawTask-master\tmp\sample_run_5f52b547-2a93-47e4-914b-08d1f72d1e7f\copyDraw_block01\tscore_{s}copyDraw_block01.mat')
+        
+        matlab_res = sio.loadmat(p,simplify_cells = True)
+        
+        theBox_mat = matlab_res['theBox']['boxPos'].T.astype(float)
+        winSize_mat = np.array([3440,1440]) # matlab just goes fullscreen - adapt to other monitors if needed
+        target_mat = matlab_res['templateLet'].astype(float)
+        pixLet_mat = matlab_res['traceLet'].astype(float) # should be int  but there are nans in there that we "need"
+        
+        pyscore = get_score_from_trace(pixLet_mat, target_mat, theBox_mat, winSize_mat, shift=False)
+        matscore = matlab_res['score']
+        
+        print(f'MATLAB: {matscore}\nPYTHON: {pyscore}')
+        
+        #output: 
+            
+            # MATLAB: 8.004574446468194
+            # PYTHON: 9.112219009818578
+            # MATLAB: 9.757498670798515
+            # PYTHON: 12.177279084773918
+            # MATLAB: 8.435895381163338
+            # PYTHON: 10.640118055976622
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- pixToIm.py
+++ pixToIm.py
@@ -7,17 +7,19 @@ Created on Tue Feb 23 12:04:11 2021
 
 import numpy as np
 from scipy.signal import convolve2d
+from scipy.spatial.distance import pdist
 
-
-### THIS IS UNFINISHED, technically, should work tho ####
-### not yet verified with matlab either ###
-
-def in_box(point,theBox,leeway=100):
+def in_box(point,theBox,leeway=100,return_size=False):
     min_x = np.min(theBox.T[0]) - leeway
     max_x = np.max(theBox.T[0]) + leeway
     
     min_y = np.min(theBox.T[1]) - leeway
     max_y = np.max(theBox.T[1]) + leeway
+    
+    
+    #skip point checking and just return the size of the box plus leeway
+    if return_size:
+        return min_x,max_x,min_y,max_y
     
     if point[0] >= min_x and point[0] <= max_x and point[1] >= min_y and point[1] <= max_y:
         return True
@@ -51,7 +53,7 @@ def conv2(x, y, mode='same'):
 
 
 
-def pixToIm(pixLett,theBoxPix,winSize,sz=50,reach=12):
+def pixToIm(pixLett,theBoxPix,winSize,sz=50,reach=12,leeway=100,shift=True):
     #values are relative to 0,0 at center of screen, will need to shift
     shift_x = winSize[0]/2
     shift_y = winSize[1]/2
@@ -60,19 +62,61 @@ def pixToIm(pixLett,theBoxPix,winSize,sz=50,reach=12):
     theBox = theBoxPix.copy()
     pixLet = pixLett.copy()
     
-    pixLet[:,0] += shift_x
-    pixLet[:,1] += shift_y
-    theBox[:,0] += shift_x
-    theBox[:,1] += shift_y
+    #dont need to shift when working with data from matlab implementation
+    if shift:
+        pixLet[:,0] += shift_x
+        pixLet[:,1] += shift_y
+        theBox[:,0] += shift_x
+        theBox[:,1] += shift_y
+    
+    
+    #this whole section feels messy, pretty sure it can be neatened up a bit 
+    #maybe done using no loops and just numpy
     
     im = np.zeros(winSize)
-    for i,(px,py) in enumerate(pixLet):
-        px = int(np.round(px))
-        py= int(np.round(py))
+    # for i,(px,py) in enumerate(pixLet):
+    #     px = int(np.round(px))
+    #     py= int(np.round(py))
         
-        #need to ignore jumps outside of the Box
-        if in_box((px,py),theBox):
-            im[px,py] = 1
+    #     try:
+    #         #need to ignore jumps outside of the Box
+    #         if in_box((px,py),theBox,leeway=leeway):
+    #             im[px,py] = 1
+    #     except IndexError:
+    #         print(im.shape)
+    #         print(px,py)
+    #         print(i)
+    #         raise IndexError    
+            
+            
+    # #now need to reduce im dimensions to those of the box
+    # min_x,max_x,min_y,max_y = in_box((0,0),theBox,leeway=leeway,return_size=True)
+    # im = im[min_x:max_x,min_y:max_y]
+    
+    
+    #detect nans and linebreaks and fix
+    if np.isnan(pixLet).any():
+        for idx in range(1,len(pixLet)):
+
+            pix = pixLet[idx-1:idx+1]         
+
+            if not np.isnan(pix[:,1]).any(): 
+                
+                #matlab and python round certain .5 numbers differently (probably floating point stuff) eg 8.5
+                #be aware
+                xyd = np.round(pdist(pix)*2)
+                xys = np.round(np.linspace(*pix,int(xyd))).astype(int)
+                
+                im[xys.T[0],xys.T[1]] += 1
+
+    else:
+        # doing int conversion here wont break nans because of the if statement above
+        im[pixLet.T[0].astype(int),pixLet.T[1].astype(int)] += 1
+        
+
+    mins = (np.min(theBox,axis=0) - leeway).astype(int)
+    maxs = (np.max(theBox,axis=0) + leeway).astype(int)
+    im[mins[0]:maxs[0],mins[1]:maxs[1]]
             
     blur = matlab_style_gauss2D(shape=(sz,sz),sigma=reach)
     gausIm = conv2(im,blur)
